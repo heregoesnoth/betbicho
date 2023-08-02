e136b21f7fc2efcbd937d50df9c3bbd23c9d4874
Lucas Gomes <lucasgms16@gmail.com>
2023-08-02 16:00:59 -0300

    first commit

---
+++ src/main/java/br/com/ada/jogobicho/JogoBichoApplication.java
@@ -0,0 +1,13 @@
+package br.com.ada.jogobicho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JogoBichoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JogoBichoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ada/jogobicho/JogoBichoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ada.jogobicho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JogoBichoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
